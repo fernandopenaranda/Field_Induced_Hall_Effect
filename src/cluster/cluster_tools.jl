a741b0fbe5c306dd5ebabd031093dae11009f6ba
Fernando Penaranda <fernandopenaranda@github.com>
2026-03-09 23:16:06 +0100

    f

--- src/cluster/cluster_tools.jl
+++ src/cluster/cluster_tools.jl
@@ -1,3 +1,5 @@
+using JLD2
+using FilePathsBase
 function slurm_conductivities(; dirj = :x, dirE = :y, dirB = :x, T = 5, evals = 1000, omega_switch = true, 
     ps_switch = true, qm_switch = true, fermi_surface = true, epsilon = 1e-5, which_mm = :orbital, integration_method = :montecarlo,
     t1 = 1, t2 = 0.5, t3 = 0.1, mumin = -3, mumax = 3, mupoints = 1, dryrun=false)
@@ -10,4 +12,68 @@ function slurm_conductivities(; dirj = :x, dirE = :y, dirB = :x, T = 5, evals = 
         $mumin $mumax $mupoints`
     dryrun && return cmd
     run(cmd)
+end
+#_________________________________________________________________________________________
+# reshape data after calculation
+
+#using LMC, JLD2 # needs to load LMC structures to approately read the preset files
+
+""" PID folder finder: e.g. PID = 0000001, 
+    `find_folder(PID)`
+ find the absolute path to PID folder regardless the data
+ store phase diagrams, drude, qah, LMCS responses... """
+find_folder(PID) = find_folder(string(PID))
+function find_folder(target::AbstractString)
+    root = pwd()
+    for (dirpath, dirnames, _) in walkdir(root)
+        if target in dirnames
+            return joinpath(dirpath, target)
+        end
+    end
+    return nothing
+end
+
+
+
+
+function postprocessing(PID)
+    destination =  homedir() * "/Projects/Field_Induced_Hall_Effect/" * PID 
+    calcfile = destination * "/merged_calculation.jld"
+    presfile = destination * "/merged_presets.jld"
+
+    
+    pid_folder =  find_folder(PID)
+
+    subfolders = filter(isdir, joinpath.(pid_folder, readdir(pid_folder)))
+
+    first_vector = nothing
+    summed_vector = nothing
+
+    for folder in subfolders
+        file = joinpath(folder, "calculation.jld")
+
+        if isfile(file)
+            data = load(file)
+
+            v1 = data["vector1"]
+            v2 = data["vector2"]
+
+            if first_vector === nothing
+                first_vector = v1
+                summed_vector = copy(v2)
+            else
+                summed_vector .+= v2
+            end
+        end
+    end
+
+    save(calcfile,
+        "vector1" => first_vector,
+        "vector2_sum" => summed_vector
+    )
+
+    cp(pid_folder * "/1/presets.jld", presfile)
+
+    println("Saved summed result to $destination")
+
 end
